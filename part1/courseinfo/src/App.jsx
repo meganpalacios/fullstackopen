e3c9ee0e5c37f31581606826e0757c19a124440f
Megan Palacios <meg2086@outlook.com>
2023-03-09 15:13:28 -0500

    1.5: course information step5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,18 +3,20 @@ import { Content } from "./components/Content/Content";
 import { Total } from "./components/Total/Total";
 
 const App = () => {
-	const course = "Half Stack application development";
-	const parts = [
-		{ name: "Fundamentals of React", exercises: 10 },
-		{ name: "Using props to pass data", exercises: 7 },
-		{ name: "State of a component", exercises: 14 },
-	];
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{ name: "Fundamentals of React", exercises: 10 },
+			{ name: "Using props to pass data", exercises: 7 },
+			{ name: "State of a component", exercises: 14 },
+		],
+	};
 
 	return (
 		<div>
-			<Header course={course}></Header>
-			<Content parts={parts} />
-			<Total parts={parts}></Total>
+			<Header course={course.name}></Header>
+			<Content parts={course.parts} />
+			<Total parts={course.parts}></Total>
 		</div>
 	);
 };
